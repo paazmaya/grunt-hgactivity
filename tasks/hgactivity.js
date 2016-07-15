8b9e2e2e2e71c98fbd7199d9fa02a2dec20cb651
Juga Paazmaya <paazmaya@yahoo.com>
2016-07-15 13:59:23 +0300

    Secure every :pig:

--- tasks/hgactivity.js
+++ tasks/hgactivity.js
@@ -2,7 +2,7 @@
  * grunt-hgactivity
  * https://github.com/paazmaya/grunt-hgactivity
  *
- * Copyright (c) Juga Paazmaya <paazmaya@yahoo.com> (http://paazmaya.fi)
+ * Copyright (c) Juga Paazmaya <paazmaya@yahoo.com> (https://paazmaya.fi)
  * Licensed under the MIT license.
  */
 'use strict';
